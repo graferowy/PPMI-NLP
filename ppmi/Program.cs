f266df0795367bc75bb2fcef9c7ec3849e9584d6
Filip Kaszczyński <graferubp@gmail.com>
2018-01-19 19:37:33 +0100

    Coocurrence Matrix
    Coocurrence Matrix generation with variable window size.

--- ppmi/Program.cs
+++ ppmi/Program.cs
@@ -11,9 +11,12 @@ namespace ppmi
         static void Main(string[] args)
         {
             string[] files = { @"text.txt" };
+            int windowSize = 4;            
             List<string> dictionary = new List<string>();
             dictionary = GenerateDictionary(files);
-            Console.WriteLine("Dictionary length: {0}", dictionary.Count);
+            Console.WriteLine("Dictionary Generated... Dictionary length: {0}", dictionary.Count);
+            int[,] coocurrenceMatrix = GenerateCoocurrenceMatrix(dictionary, files, windowSize);            
+            Console.WriteLine("Coocurrence Matrix Generated...");
         }
 
         static List<string> GenerateDictionary(string[] files)
@@ -31,6 +34,32 @@ namespace ppmi
                                     tempDictionary.Add(currentWord);
                             }
             return tempDictionary;
+        }
+
+        static int[,] GenerateCoocurrenceMatrix(List<string> dictionary, string[] files, int windowSize)
+        {
+            int[,] tempMatrix = new int[dictionary.Count, dictionary.Count];
+            Queue<string> windowWords = new Queue<string>();
+            Regex rgx = new Regex("[0-9$*+,:=?/\\[\\]@#|'<>.^\"*()%!-]");
+            foreach (var file in files)
+                if (File.Exists(file))
+                    using (StreamReader sr = new StreamReader(file))
+                        while (sr.Peek() >= 0)
+                            foreach (var word in sr.ReadLine().Split(' '))
+                            {
+                                string currentWord = rgx.Replace(word, "").ToLower();
+                                if(dictionary.Contains(currentWord)) { 
+                                    foreach (var previousWord in windowWords)
+                                    {
+                                        tempMatrix[dictionary.IndexOf(currentWord), dictionary.IndexOf(previousWord)] += 1;
+                                        tempMatrix[dictionary.IndexOf(previousWord), dictionary.IndexOf(currentWord)] += 1;
+                                    }
+                                    windowWords.Enqueue(currentWord);
+                                    if (windowWords.Count > windowSize)
+                                        windowWords.Dequeue();
+                                }
+                            }
+            return tempMatrix;
         }
     }
 }
