b2196197f3a80c620b30d5eaeee7a047069f5afb
Filip Kaszczyński <graferubp@gmail.com>
2018-01-19 20:20:15 +0100

    PPMI Matrix
    Positive PMI Matrix generation.
    Time measurment for each step.

--- ppmi/Program.cs
+++ ppmi/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 namespace ppmi
 {
@@ -10,13 +11,27 @@ namespace ppmi
     {
         static void Main(string[] args)
         {
+            Stopwatch timer = new Stopwatch();
             string[] files = { @"text.txt" };
+            int[,] coocurrenceMatrix;
+            double[,] ppmiMatrix;
             int windowSize = 4;            
             List<string> dictionary = new List<string>();
+
+            timer.Start();
             dictionary = GenerateDictionary(files);
-            Console.WriteLine("Dictionary Generated... Dictionary length: {0}", dictionary.Count);
-            int[,] coocurrenceMatrix = GenerateCoocurrenceMatrix(dictionary, files, windowSize);            
-            Console.WriteLine("Coocurrence Matrix Generated...");
+            timer.Stop();
+            Console.WriteLine("Dictionary Generated in {0}s. Dictionary length: {1}", timer.Elapsed, dictionary.Count);
+
+            timer.Restart();
+            coocurrenceMatrix = GenerateCoocurrenceMatrix(dictionary, files, windowSize);
+            timer.Stop();
+            Console.WriteLine("Coocurrence Matrix Generated in {0}s.", timer.Elapsed);
+
+            timer.Restart();
+            ppmiMatrix = CalculatePpmiMatrix(coocurrenceMatrix);
+            timer.Stop();
+            Console.WriteLine("PPMI Matrix Generated in {0}s.", timer.Elapsed);
         }
 
         static List<string> GenerateDictionary(string[] files)
@@ -60,6 +75,47 @@ namespace ppmi
                                 }
                             }
             return tempMatrix;
+        }
+
+        static double[,] CalculatePpmiMatrix(int[,] coocMatrix)
+        {
+            double[,] tempPpmiMatrix = new double[coocMatrix.GetLength(0), coocMatrix.GetLength(1)];
+            int denominator = GetDenominator(coocMatrix);
+            for (int i = 0; i < coocMatrix.GetLength(0); i++)
+                for (int j = i; j < coocMatrix.GetLength(1); j++)
+                {
+                    tempPpmiMatrix[i, j] = CalculatePpmi(coocMatrix, i, j, denominator);
+                    tempPpmiMatrix[j, i] = tempPpmiMatrix[i, j];
+                }
+            return tempPpmiMatrix;
+        }
+
+        static int GetDenominator(int[,] matrix)
+        {
+            int result = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    result += matrix[i, j];
+            return result;
+        }
+
+        static double CalculatePpmi(int[,] coocMatrix, int i, int j, int denominator)
+        {
+            double p, p1, p2, p3;
+            double p1numerator = 0, p2numerator = 0, p3numerator = 0;
+
+            p1numerator = coocMatrix[i, j];
+            for (int x = 0; x < coocMatrix.GetLength(0); x++)
+            {
+                p2numerator += coocMatrix[i, x];
+                p3numerator += coocMatrix[x, j];
+            }
+            p1 = p1numerator / denominator;
+            p2 = p2numerator / denominator;
+            p3 = p3numerator / denominator;
+
+            p = Math.Log((p1 / (p2 * p3)), 2);
+            return (p < 0 || Double.IsNaN(p)) ? 0 : p;
         }
     }
 }
